04b3e0f61ea8755e6af315d8f7a9546ec5a0ea3a
Lenovo <Lenovo@DESKTOP-O6UJNUF>
2026-02-22 22:51:52 +0300

    Add project files.

---
+++ assignment 1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("enter the bill amount : ");
+
+double amount = Convert.ToDouble(Console.ReadLine());
+
+if (amount >= 500)
+{
+    double amount_1 = amount * 30/100;
+    Console.WriteLine($"final price :{amount_1} ");
+}
+else if (amount >= 300 && amount <= 499)
+{
+    float amount_1 = (float) amount * 10/100  ;
+    Console.WriteLine($"final price :{amount_1} ");
+
+}
+else if (amount <300)
+{
+    float amount_1 =  (float) amount;
+    Console.WriteLine($"final price :{amount_1} ");
+
+}
+
